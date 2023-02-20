21b52b8df2ad2b58d8b2cf56a4715622eaf4bb3f
Presh Amuh <amuhprecious@gmail.com>
2023-02-21 00:16:37 +0100

    task 3

---
+++ index.js
@@ -0,0 +1,26 @@
+// Question 3
+
+// science = Physics, Chemistry, Biology, Technical-Drawing
+// social science = Accounting, Commerce, Marketing, Geography
+//arts = Government, Economics, Literature, History
+// general = English, Mathematics
+
+
+let studentSubject = 'Science'
+
+
+if (studentSubject = "science"){
+    console.log("Physics, Chemistry, Biology, Technical-Drawing")
+}
+
+else if (studentSubject = "social science"){
+    console.log("Accounting, Commerce, Marketing, Geography")
+}
+
+else if (studentSubject = "arts"){
+    console.log("Government, Economics, Literature, History")
+}
+
+else {
+    console.log("English, Mathematics")
+}
