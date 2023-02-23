a3129798bda47a555c46470cafdd1e5b15027ac1
Presh Amuh <amuhprecious@gmail.com>
2023-02-23 22:28:17 +0100

    revisiting answers to ques. 5

--- index.js
+++ index.js
@@ -24,3 +24,31 @@ else if (studentSubject = "arts"){
 else {
     console.log("English, Mathematics")
 }
+
+
+// Question 5
+
+let num = 50;;
+let pwr = 1;
+const arr = [];
+while (pwr < num) {
+    pwr *= 2;
+    if (Math.abs(num - pwr / 2) < Math.abs(num - pwr)) {
+    }
+    arr.push(pwr);
+}
+
+let result;
+for (let a = 0; a < arr.length; a++) {
+  let firstnum = arr[arr.length - 2];
+  let numone = num - firstnum;
+  let lastnum = arr[arr.length - 1];
+  let numlast = num - lastnum;
+  
+  if (numone <= Math.abs(numlast)) {
+    result = firstnum;
+  } else {
+    result = lastnum;
+  }
+}
+console.log(`The number ${result} is the power of 2 nearest to ${num}`)
